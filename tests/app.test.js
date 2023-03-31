cbc2fbcda125cc9e5123937e321f244934089c42
Edward Trigos <edward.trigos@nivelics.co>
2023-03-31 16:35:35 -0500

    [test] sum with large numbers

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,9 +5,15 @@ describe('app', () => {
     expect(app(2, 5)).toBe(7);
   });
   it('should sum 15 and 7 and get 22', () => {
-    expect(app(2, 5)).toBe(7);
+    expect(app(15, 7)).toBe(22);
   });
   it('should sum 25 and 50 and get 75', () => {
-    expect(app(2, 5)).toBe(7);
+    expect(app(25, 50)).toBe(75);
+  });
+  it('should sum 2500 and 5000 and get 7500', () => {
+    expect(app(2500, 5000)).toBe(7500);
+  });
+  it('should sum 125000 and 75000 and get 200000', () => {
+    expect(app(125000, 75000)).toBe(200000);
   });
 });
